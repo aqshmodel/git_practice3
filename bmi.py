97bd285b50ce41490fb8069deef4d286325f5ff7
takahiro.tsukada <aqsh.reprom@gmail.com>
2019-11-03 17:01:07 +0900

    メソッドの名前の違和感を修正
    - メソッドculculate_bmiを属性valueに変更
    - クラスのメソッドを属性に修正

--- bmi.py
+++ bmi.py
@@ -18,18 +18,16 @@ class BMI:
     def __init__(self, height, weight):
         self.height = height
         self.weight = weight
-
-    def culculate_bmi(self):
-        return self.weight / (self.height ** 2)
+        self.value  = self.weight / (self.height ** 2)
 
 
 # BMIのクラスのインスタンス化
 hibiki_bmi = BMI(height=1.80, weight=67.0)
 print('Hibiki')
 print(hibiki_bmi.height, hibiki_bmi.weight)
-print(hibiki_bmi.culculate_bmi())
+print(hibiki_bmi.value)
 
 noriya_bmi = BMI(height=1.78, weight=75.0)
 print('Ohira')
 print(noriya_bmi.height, noriya_bmi.weight)
-print(noriya_bmi.culculate_bmi())
+print(noriya_bmi.value)
