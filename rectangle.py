074cda7ff0718446e43d9333fefe809a44ed7dae
takahiro.tsukada <aqsh.reprom@gmail.com>
2019-11-04 00:53:01 +0900

    課題2 長方形の面積と対角線長を計算するclass完成
    - 少数点第2位まで表示できるように修正

--- rectangle.py
+++ rectangle.py
@@ -5,15 +5,17 @@
 '''
 import math
 
+
 class Rectangle:
     def __init__(self, height, width):
         self.height = height
         self.width = width
+
     def area(self):
-        return self.height * self.width
+        return f'{self.height * self.width:.2f}'
 
     def diagonal(self):
-        return math.sqrt((self.height ** 2) + (self.width ** 2))
+        return round(math.sqrt((self.height ** 2) + (self.width ** 2)), 2)
 
 
 # height5、width6の長方形
