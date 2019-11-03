82ebd454f58a3ea27d50cbbccb606e84773c5c53
takahiro.tsukada <aqsh.reprom@gmail.com>
2019-11-03 18:27:19 +0900

    課題2 動作OK
    - 長方形の面積、対角線長の表示をするクラスを完成
    - まだ小数点2位までの表示に未対応

--- rectangle.py
+++ rectangle.py
@@ -3,6 +3,7 @@
 長方形の面積を計算するメソッド、areaが必要
 長方形の対角線の長さを計算するメソッド、diagonalが必要
 '''
+import math
 
 class Rectangle:
     def __init__(self, height, width):
@@ -12,16 +13,17 @@ class Rectangle:
         return self.height * self.width
 
     def diagonal(self):
-        import math
-        return math.sprt((self.height ** 2) + (self.width ** 2))
+        return math.sqrt((self.height ** 2) + (self.width ** 2))
 
 
 # height5、width6の長方形
 rectangle1 = Rectangle(height=5, width=6)
+print('高さ5、幅6の長方形')
 print(rectangle1.area())  # 30.00
-rectangle1.diagonal()  # 7.81
+print(rectangle1.diagonal())  # 7.81
 
 # height3、width3の長方形
 rectangle2 = Rectangle(height=3, width=3)
+print('高さ3、幅3の長方形')
 print(rectangle2.area())  # 9.00
-#rectangle2.diagonal()  # 4.24
+print(rectangle2.diagonal())  # 4.24
